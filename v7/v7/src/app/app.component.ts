cbbbcf5a205d4b3771ba96fb2f2369fc9d22eb51
yuriyoon1009 <yuriyoon1009@gmail.com>
2018-01-16 15:07:24 +0900

    delete v7 folder inside v7

--- v7/v7/src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<todo-container></todo-container>'
-})
-export class AppComponent {}
--- v7/v7/src/app/todos/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './todo-container.component';
-export * from './todo-form.component';
-export * from './todo-nav.component';
-export * from './todo-list.component';
-export * from './todo-footer.component';
